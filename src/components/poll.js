78c841e6c3ab9f432b4b8fcd95e0073c4dec6e0c
PushpendraSingh <pushpendrasingh2306@gmail.com>
2022-04-16 08:02:23 +0530

    changes

--- src/components/poll.js
+++ src/components/poll.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import {React, useEffect, useState} from 'react'
+import config from "../config"
+import axios from "axios"
 
 function Poll() {
+    const [data, setData] = useState([]);
+
+useEffect(() => {
+
+    function check(){
+        axios.get("http://localhost:3001/v1/getAllPolls")
+        .then((res) => {
+            console.log("res", res);
+            if(res.status === 200){
+                setData(res.data)
+                console.log("dat",data);
+            }
+        })
+    };
+
+    check();
+   
+}, [data])
+
     return (
         <div>
             <h3>Poll</h3>
             <div className="cardCover">
+
+            {data.length ? [] : data.map((item) => {
+                    return(
                 <div className="card">
                 <div className="question">
-                <h4>Poll Question</h4>
+                <h4>{item.question}</h4>
                     <ul>
-                        <li>options</li>
+                    {item.options.map((tile) => {
+                      return (<li>{tile.option}</li>)
+                    })}
                     </ul>
                 </div>
                 <div className="result">
@@ -18,7 +44,9 @@ function Poll() {
                   <tr><td>option1</td><td>10</td></tr>
                   </table>
                 </div>
-                </div>
+                </div>)
+                })}
+
             </div>
         </div>
     )
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = {
+    apiUrl: "localhost:3001"
+}
+
+export default config;
